ea929f7e2985b7f87dfa19b8ec4dbe989b0e86d5
Jeffrey Sanford <jeffreysanford@gmail.com>
2016-01-20 20:44:30 -0800

    adding HTML5 presentation -- ini commit

---
+++ js/javascript.js
@@ -0,0 +1,37 @@
+function skipTheVideo() {
+    var videoWrapper = document.getElementById("video-wrapper");
+//    videoWrapper.pause();
+    $("#video-content").addClass('hidden');
+    $("#skip-video").addClass('hidden');
+    $("#page-template").removeClass('hidden');                
+    console.log("transition finished");
+}
+
+function playTheVideo() {
+    console.log("play the video fired");
+    // play the video full screen
+    $("#video-wrapper").removeClass('hidden');
+    $("#skip-video").removeClass('hidden');
+    $("#page-template").addClass('hidden');   
+    var videoWrapper = document.getElementById("video-wrapper");
+    var pageTemplate = document.getElementById('page-template');
+    videoWrapper.currentTime = 0;
+    videoWrapper.play()
+    videoWrapper.onended = function transitionToNewPage() {
+        $("#video-wrapper").addClass('hidden');
+        $("#page-template").removeClass('hidden');                
+    console.log("transition finished");
+    };
+}  // ends the playTheVideo function    
+
+window.onload = function onLoadFunction () {
+    
+    // play the video on DOM load
+    
+    playTheVideo();
+
+    // register click events for buttons
+    
+    //  Now set the display to none for the video element
+        
+};
