d5c78cc1fb8f5dc17f7ef563c04389af89fb4e34
Jeffrey Sanford <jeffreysanford@gmail.com>
2016-01-20 22:55:08 -0800

    adding the audio tracks

--- js/javascript.js
+++ js/javascript.js
@@ -1,21 +1,25 @@
 function skipTheVideo() {
     var videoWrapper = document.getElementById("video-wrapper");
+    var audioWrapper = document.getElementById("audio-wrapper");
     videoWrapper.pause();
 //    videoWrapper.pause();
     $("#video-content").addClass('hidden');
     $("#skip-video").addClass('hidden');
-    $("#page-template").removeClass('hidden');                
+    $("#page-template").removeClass('hidden');
+    audioWrapper.play();
     console.log("transition finished");
 }
 
 function playTheVideo() {
+    var audioWrapper = document.getElementById("audio-wrapper");
+    var videoWrapper = document.getElementById("video-wrapper");
+    var pageTemplate = document.getElementById('page-template');
     console.log("play the video fired");
     // play the video full screen
     $("#video-wrapper").removeClass('hidden');
     $("#skip-video").removeClass('hidden');
-    $("#page-template").addClass('hidden');   
-    var videoWrapper = document.getElementById("video-wrapper");
-    var pageTemplate = document.getElementById('page-template');
+    $("#page-template").addClass('hidden');
+    audioWrapper.pause();   
     videoWrapper.currentTime = 0;
     videoWrapper.play();
     videoWrapper.onended = function transitionToNewPage() {
