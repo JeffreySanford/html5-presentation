09cfca489423992b11b8c03e835eac577ba756d1
Jeffrey Sanford <jeffreysanford@gmail.com>
2016-01-21 10:05:08 -0800

    fix the skip button

--- js/javascript.js
+++ js/javascript.js
@@ -1,10 +1,13 @@
 function skipTheVideo() {
     var videoWrapper = document.getElementById("video-wrapper");
     var audioWrapper = document.getElementById("audio-wrapper");
+    
+    // turning off the video if not finsihed
     videoWrapper.pause();
-//    videoWrapper.pause();
     $("#video-content").addClass('hidden');
     $("#skip-video").addClass('hidden');
+
+    // moving to the main page and turning on audio
     $("#page-template").removeClass('hidden');
     audioWrapper.play();
     console.log("transition finished");
@@ -15,28 +18,30 @@ function playTheVideo() {
     var videoWrapper = document.getElementById("video-wrapper");
     var pageTemplate = document.getElementById('page-template');
     console.log("play the video fired");
-    // play the video full screen
+
+    // remove the video elements and hide the page now
     $("#video-wrapper").removeClass('hidden');
     $("#skip-video").removeClass('hidden');
     $("#page-template").addClass('hidden');
-    audioWrapper.pause();   
+
+    audioWrapper.pause();
+
+    // play the video from the beginning 
     videoWrapper.currentTime = 0;
     videoWrapper.play();
+
+    // when finsihed, move to the main page
     videoWrapper.onended = function transitionToNewPage() {
         $("#video-wrapper").addClass('hidden');
-        $("#page-template").removeClass('hidden');                
+        $("#page-template").removeClass('hidden');
+
+        // hide the skip video and start the ambience 
+        $("#skip-video").addClass('hidden');
+        audioWrapper.play();            
     console.log("transition finished");
     };
 }  // ends the playTheVideo function    
 
 window.onload = function onLoadFunction () {
-    
-    // play the video on DOM load
-    
-    playTheVideo();
-
-    // register click events for buttons
-    
-    //  Now set the display to none for the video element
-        
+    playTheVideo();        
 };
