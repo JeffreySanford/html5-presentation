a17e38746032a9b01f5c898a9983adc3f70a0d47
Jeffrey Sanford <jeffreysanford@gmail.com>
2016-01-21 22:55:23 -0800

    refactored into template driven design with handlesbars.  Add external json file.  Tested with JSLINT and JSHINT.

--- js/javascript.js
+++ js/javascript.js
@@ -1,47 +1,81 @@
-function skipTheVideo() {
-    var videoWrapper = document.getElementById("video-wrapper");
-    var audioWrapper = document.getElementById("audio-wrapper");
-    
-    // turning off the video if not finsihed
-    videoWrapper.pause();
-    $("#video-content").addClass('hidden');
-    $("#skip-video").addClass('hidden');
+/*global $ */
+(function IFEE() {
+    'use strict';
+    function loadMainPage() {
+        var videoWrapper = document.getElementById("video-wrapper");
+        var audioWrapper = document.getElementById("audio-wrapper");
 
-    // moving to the main page and turning on audio
-    $("#page-template").removeClass('hidden');
-    audioWrapper.play();
-    console.log("transition finished");
-}
+        //Check for fullscreen
+        // are we full-screen?
+        if (document.fullscreenElement || document.webkitFullscreenElement || document.mozFullScreenElement || document.msFullscreenElement) {
+            // exit full-screen
+            if (document.exitFullscreen) {
+                document.exitFullscreen();
+            } else if (document.webkitExitFullscreen) {
+                document.webkitExitFullscreen();
+            } else if (document.mozCancelFullScreen) {
+                document.mozCancelFullScreen();
+            } else if (document.msExitFullscreen) {
+                document.msExitFullscreen();
+            }
+        }
 
-function playTheVideo() {
-    var audioWrapper = document.getElementById("audio-wrapper");
-    var videoWrapper = document.getElementById("video-wrapper");
-    var pageTemplate = document.getElementById('page-template');
-    console.log("play the video fired");
+        // turning off the video if not finsihed
+        videoWrapper.pause();
+        videoWrapper.currentTime = 0;
 
-    // remove the video elements and hide the page now
-    $("#video-wrapper").removeClass('hidden');
-    $("#skip-video").removeClass('hidden');
-    $("#page-template").addClass('hidden');
-
-    audioWrapper.pause();
-
-    // play the video from the beginning 
-    videoWrapper.currentTime = 0;
-    videoWrapper.play();
-
-    // when finsihed, move to the main page
-    videoWrapper.onended = function transitionToNewPage() {
-        $("#video-wrapper").addClass('hidden');
-        $("#page-template").removeClass('hidden');
-
-        // hide the skip video and start the ambience 
+        $("video").addClass('hidden');
         $("#skip-video").addClass('hidden');
-        audioWrapper.play();            
-    console.log("transition finished");
-    };
-}  // ends the playTheVideo function    
 
-window.onload = function onLoadFunction () {
-    playTheVideo();        
-};
+        // moving to the main page and turning on audio
+        $("#mainpage-container").removeClass('hidden');
+        $("#navigation").removeClass('hidden');
+
+        // play the udio track from the beginnning
+        audioWrapper.currentTime = 0;
+        audioWrapper.play();
+    }
+
+    function playTheVideo() {
+        var audioWrapper = document.getElementById("audio-wrapper");
+        var videoWrapper = document.getElementById("video-wrapper");
+
+        //allow for skipping the video
+        $("#skip-video").on("click", function () {
+            loadMainPage();
+        });
+
+        // try fullscreen
+        if (videoWrapper.requestFullscreen) {
+            videoWrapper.requestFullscreen();
+        } else if (videoWrapper.webkitRequestFullscreen) {
+            videoWrapper.webkitRequestFullscreen();
+        } else if (videoWrapper.mozRequestFullScreen) {
+            videoWrapper.mozRequestFullScreen();
+        } else if (videoWrapper.msRequestFullscreen) {
+            videoWrapper.msRequestFullscreen();
+        }
+
+        // remove the video elements and hide the page now
+        $("#video-wrapper").removeClass('hidden');
+        $("#skip-video").removeClass('hidden');
+        $("#mainpage-container").addClass('hidden');
+        $("#navigation").addClass('hidden');
+
+        audioWrapper.pause();
+
+        // play the video from the beginning
+        videoWrapper.play();
+
+        // when finsihed, move to the main page
+        videoWrapper.onended = function transitionToNewPage() {
+            videoWrapper.currentTime = 0;
+            loadMainPage();
+        };
+    }  // ends the playTheVideo function
+
+    $(document).ready(function () {
+        //start the video
+        playTheVideo();
+    });
+}());
